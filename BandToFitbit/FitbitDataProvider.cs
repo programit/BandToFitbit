6cb75e03ae301fc7875de54fae0a78ac74505485
John Morman <morman@outlook.com>
2017-01-18 19:32:37 -0800

    Delete previous entry for day if it already exists so we can recreate with potentially better data

--- BandToFitbit/FitbitDataProvider.cs
+++ BandToFitbit/FitbitDataProvider.cs
@@ -18,7 +18,36 @@ namespace BandToFitbit
 
         private const string PublishStepsUrl = "https://api.fitbit.com/1/user/-/activities.json?activityId=90013&startTime=00%3A00&durationMillis=86400000&date={0}&distance={1}&distanceUnit=steps";
 
+        private const string DeleteStepsUrl = "https://api.fitbit.com/1/user/-/activities/{0}.json";
+
         public async Task PublishSteps(int steps)
+        {
+            HttpResponseMessage responseMessage = await this.TryPublish(steps);
+            if(responseMessage.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return;
+            }
+
+            string responseString = await responseMessage.Content.ReadAsStringAsync();
+            FitbitPublishResponseContract publishResponse = JsonConvert.DeserializeObject<FitbitPublishResponseContract>(responseString);
+            await this.DeleteSteps(publishResponse.activityLog.logId);
+            await this.TryPublish(steps);
+        }
+
+        private async Task DeleteSteps(long logId)
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(string.Format(FitbitDataProvider.DeleteStepsUrl, logId)),
+                Method = HttpMethod.Post,
+            };
+
+            request.Headers.Add(FitbitDataProvider.AuthHeader, this.CreateAuthHeader(Secrets.FitbitAuthToken));
+            await client.SendAsync(request);
+        }
+
+        private async Task<HttpResponseMessage> TryPublish(int steps)
         {
             HttpClient client = new HttpClient();
             HttpRequestMessage request = new HttpRequestMessage()
@@ -29,8 +58,7 @@ namespace BandToFitbit
 
             request.Headers.Add(FitbitDataProvider.AuthHeader, this.CreateAuthHeader(Secrets.FitbitAuthToken));
 
-            HttpResponseMessage response = await client.SendAsync(request);
-            string result = await response.Content.ReadAsStringAsync();
+            return  await client.SendAsync(request);            
         }
 
         public async Task<FitbitGetStepsDataContract> GetSteps()
---
+++ BandToFitbit/FitbitPublishResponseContract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandToFitbit
+{
+    public class FitbitPublishResponseContract
+    {
+        public ActivityLog activityLog { get; set; }
+    }
+
+    public class ActivityLog
+    {
+        public int activityId { get; set; }
+        public int activityParentId { get; set; }
+        public string activityParentName { get; set; }
+        public int calories { get; set; }
+        public string description { get; set; }
+        public double distance { get; set; }
+        public int duration { get; set; }
+        public bool hasStartTime { get; set; }
+        public bool isFavorite { get; set; }
+        public string lastModified { get; set; }
+        public long logId { get; set; }
+        public string name { get; set; }
+        public string startDate { get; set; }
+        public string startTime { get; set; }
+        public int steps { get; set; }
+    }
+}
